0c85763d687e1e0a774a150cfda36d239f7254f0
abanob282 <abanobn4@gmail.com>
2022-09-06 16:30:24 +0200

    6

--- src/app/rental-cars-page/rental-cars-page.module.ts
+++ src/app/rental-cars-page/rental-cars-page.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CountdownModule } from 'ngx-countdown';
 
 import { RentalCarsPageRoutingModule } from './rental-cars-page-routing.module';
 
@@ -8,7 +9,8 @@ import { RentalCarsPageRoutingModule } from './rental-cars-page-routing.module';
   declarations: [],
   imports: [
     CommonModule,
-    RentalCarsPageRoutingModule
+    RentalCarsPageRoutingModule,
+    CountdownModule
   ]
 })
 export class RentalCarsPageModule { }
